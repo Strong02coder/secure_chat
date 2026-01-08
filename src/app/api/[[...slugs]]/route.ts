38f5fd211594509bb2b2f10bab63821db3fb741e
Aryan Agrawal <strongmozilla0216@gmail.com>
2026-01-09 00:13:29 +0530

    feat: integrate Upstash Redis for room management and add room creation functionality

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,6 +1,22 @@
-import { Elysia } from "elysia";
+import { redis } from "@/lib/redis";
+import { Elysia, t } from "elysia";
+import { nanoid } from "nanoid";
 
-const app = new Elysia({ prefix: "/api" }).get("/user", { name: "Elysia" })
+const ROOM_TTL_SECONDS = 60 * 30; // 30 minutes
+
+const rooms = new Elysia({prefix: "/room"}).post("/create", async () => {
+  const roomId = nanoid();
+  await redis.hset(`meta:${roomId}`, {
+    connected: [],
+    createdAt: Date.now(),
+  })
+
+  await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS); // 30 minutes
+
+  return { roomId };
+});
+
+const app = new Elysia({ prefix: "/api" }).use(rooms)
 
 export const GET = app.fetch;
 export const POST = app.fetch;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 "use client";
+import { client } from "@/lib/client";
+import { useMutation } from "@tanstack/react-query";
 import { nanoid } from "nanoid";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const ANIMALS = ["Lion", "Tiger", "Bear", "Wolf", "Eagle", "Shark", "Panther", "Leopard", "Cheetah", "Falcon", "Hawk", "Phoenix", "Dragon",
@@ -16,6 +19,7 @@ const generateUsername = () => {
 
 export default function Home() {
   const [username, setUsername] = useState("");
+  const router = useRouter();
 
   useEffect(() => {
     const main = () => {
@@ -32,6 +36,16 @@ export default function Home() {
     }
     main();
   }, []);
+
+  const { mutate: createRoom } = useMutation({
+    mutationFn: async () => {
+      const res = await client.room.create.post();
+      
+      if (res.status === 200) {
+        router.push(`/room/${res.data?.roomId}`);
+      }
+    }
+  })
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4">
@@ -53,7 +67,7 @@ export default function Home() {
               </div>
             </div>
 
-            <button className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50 rounded-xl">
+            <button onClick={() => createRoom()} className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50 rounded-xl">
               CREATE SECURE ROOM
             </button>
           </div>
