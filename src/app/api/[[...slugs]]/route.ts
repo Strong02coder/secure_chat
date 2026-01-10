c7814b186e863b0637c76e7e216ac6ca7bb3d147
Aryan Agrawal <strongmozilla0216@gmail.com>
2026-01-10 22:16:35 +0530

    feat: add realtime messaging support with Upstash and Zod validation

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,22 +1,43 @@
 import { redis } from "@/lib/redis";
 import { Elysia, t } from "elysia";
 import { nanoid } from "nanoid";
+import { authMiddleware } from "./auth";
+import { z } from "zod";
 
 const ROOM_TTL_SECONDS = 60 * 30; // 30 minutes
 
-const rooms = new Elysia({prefix: "/room"}).post("/create", async () => {
+const rooms = new Elysia({ prefix: "/room" }).post("/create", async () => {
   const roomId = nanoid();
   await redis.hset(`meta:${roomId}`, {
     connected: [],
     createdAt: Date.now(),
-  })
+  });
 
   await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS); // 30 minutes
 
   return { roomId };
 });
 
-const app = new Elysia({ prefix: "/api" }).use(rooms)
+const messages = new Elysia({ prefix: "/messages" })
+  .use(authMiddleware)
+  .post("/", async ({ body, auth }) => {
+    const { sender, text } = body
+
+    const roomExits = await redis.exists(`meta:${auth.roomId}`)
+
+    if (!roomExits) {
+      throw new Error("Room does not exist");
+    }
+  }, 
+  {
+    query: z.object({ roomId: z.string() }),
+    body: z.object({
+      sender: z.string().max(100),
+      text: z.string().max(1000),
+    })
+  });
+
+const app = new Elysia({ prefix: "/api" }).use(rooms).use(messages);
 
 export const GET = app.fetch;
 export const POST = app.fetch;
